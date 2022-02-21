db0509875810f43a50771c82b84b1571249bf368
Md. Foysal <muhammadfoysal2448@gmail.com>
2022-02-21 16:43:16 +0600

    lecture1

---
+++ bin/first_one.dart
@@ -0,0 +1,49 @@
+// // // void main (){int variableNameId=10;
+// // //
+// // // String workstationName='foysal';
+// // //
+// // // double resultMarks=5.7;
+// // //
+// // // bool projectType=true;
+// // //
+// // // List userAmount=[1,2,3];
+// // //
+// // // Map userArea={jamalpur,mymenshing,dhaka};}
+// // //
+// //
+// //
+// // void main () {String collegeName='Islampur university';
+// // int studentRoll=1-99;
+// // double studentAmount=99;
+// // bool universityIsOpen=true;
+// //
+// // print ('Name: $collegeName Roll:$studentRoll  Amount: $studentAmount IsOpen: $universityIsOpen');
+// // print ('Name: $collegeName \nRoll:$studentRoll  \nAmount: $studentAmount IsOpen: $universityIsOpen');}
+// //
+//
+// // void main(){int value1=100;
+// // int value2 =100;
+// //
+// // int sum=value1+value2;
+// // print('$sum');
+// // }
+// void main(){int value1=200;
+// int value2=100;
+//
+// int sum=value1-value2;
+// print('$sum');
+// }
+// void main(){
+//   int value1=5;
+//   int value2=4;
+//   int sum=value1*value2;
+//   print('$sum');
+// }
+
+void main(){
+  int value1=200;
+  int value2=4;
+  double sum=value1/value2;
+  print('$sum');
+}
+
